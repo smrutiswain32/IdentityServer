d6543c8704d4124225b1ff1b2c29ce6daa2261ed
smruti swain <smrutiswain35@outlook.com>
2020-11-19 18:25:15 +0530

    Add project files.

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,52 @@
+﻿using IdentityServer4.Models;
+
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiScope> GetApiScope()
+        {
+            return new List<ApiScope>
+            {
+                new ApiScope("apiscope")
+
+            };
+        }
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource()
+                {
+                    Name="myresourceapi",
+                    DisplayName="My Resource API",
+                    Scopes = new List<string>
+                    {
+                        "apiscope",
+
+                    }
+                },
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                // for public api
+                new Client
+                {
+                    ClientId = "secret_client_id",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "apiscope" }
+                },
+            };
+        }
+    }
+}
