eb8406b53c9a5d8469b44adbeec4ab9de0ac0dd3
smruti swain <smrutiswain35@outlook.com>
2020-11-19 18:27:05 +0530

    first commit

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -25,7 +25,6 @@ namespace IdentityServer
                     Scopes = new List<string>
                     {
                         "apiscope",
-
                     }
                 },
             };
